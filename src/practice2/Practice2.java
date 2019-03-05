325691edb7330c97e5aea84eea8dc64e20c7d790
Breshna-Rahimzy-Sheridan <rahimzy@sheridancollege.ca>
2019-03-04 22:21:25 -0500

    initializing number.

---
+++ src/practice2/Practice2.java
@@ -0,0 +1,36 @@
+/*
+ * Breshna Rahimzy
+ * Student ID: 991487145
+ *     SYST10199 - WEB Programming
+ */
+package practice2;
+
+public class Practice2 {
+
+    public static void main(String[] args) {
+           int num = 10;
+        System.out.println(num);
+//        A cc = new A();
+//        cc.setName("Fred");
+//        cc.setAge(12);
+//        System.out.println("cc name is: " + cc.getName()+ "\t"+"Age is:" + "\t"+ cc.getAge());
+    }
+    
+}
+//class A{
+//    private String name;
+//    private int age;
+//
+//public void setName(String givenName){
+//this.name = givenName;
+//}
+//public String getName(){
+//    return this.name;
+//}
+//public void setAge(int givenAge){
+//    this.age = givenAge;
+//}
+//public int getAge(){
+//    return this.age;
+//}
+//}
